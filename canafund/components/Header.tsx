485bfd0939c9c4b04ef6ef7c0e5b08bcedb31af5
iliacodes <6ac177ed@protonmail.com>
2023-08-10 13:41:50 -0700

    created components for front-ent and starter contract

---
+++ canafund/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Header = () => {
+  return (
+    <div>
+      <Image src="/Canafund.png" alt="HeaderLogo" width={200} height={200} />
+    </div>
+  )
+}
+
+export default Header
---
+++ canafund/components/index.ts
@@ -0,0 +1,6 @@
+import Nav from "./Nav";
+import Footer from "./Footer";
+import Header from "./Header";
+import Menu from "./Menu";
+
+export { Nav, Header, Footer, Menu };
